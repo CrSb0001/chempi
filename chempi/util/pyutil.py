371d984b675ac9b27b500e3beb824c258073c041
CrSb0001 <127436709+CrSb0001@users.noreply.github.com>
2025-05-09 10:06:11 -0500

    Create pyutil.py

---
+++ chempi/util/pyutil.py
@@ -0,0 +1,51 @@
+'''
+General utils and exceptions
+'''
+
+from functools import wraps
+
+__url__ = '' # Remove when __url__ is setup in ..
+
+from .deprecation import Deprecation
+
+def identity(x):
+    '''
+    The identity function.
+
+    Examples
+    ==========
+    >>> identity(-5)
+    -5
+    >>> identity(6)
+    6
+    '''
+    return x
+
+class NoConvergence(Exception):
+    pass
+
+class ChemPiDeprecationWarning(DeprecationWarning):
+    pass
+
+def deprecated(*args, **kwargs):
+    '''
+    Helper to :class: `Deprecation` for using ChemPiDeprecationWarning.
+    '''
+    return Deprecation(*args, issues_url = lambda s: __url__ + '/issues/' + s.strip('gh-'), warning = ChemPiDeprecationWarning, **kwargs)
+
+def memoize(max_nargs = None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args):
+            if max_nargs is not None and len(args) > max_nargs:
+                raise ValueError('Memoization error.')
+            
+            if args not in wrapper.results:
+                wrapper.results[args] = func(*args)
+            
+            return wrapper.results[args]
+        
+        wrapper.results = {}
+        return wrapper
+    
+    return decorator
