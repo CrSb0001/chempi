fe4b0615f5554c7c36833db033d06041491ddf60
CrSb0001 <127436709+CrSb0001@users.noreply.github.com>
2025-05-12 17:26:30 -0500

    Update results.py

--- chempi/parser/results.py
+++ chempi/parser/results.py
@@ -111,3 +111,146 @@ class ParseResults:
             
             else:
                 self._name = name
+    
+    def __getitem__(self, idx):
+        if isinstance(idx, (int, slice)):
+            return self._toklist[idx]
+        
+        if idx not in self._all_names:
+            return self._tokdict[idx][-1][0]
+        
+        return ParseResults([v[0] for v in self._tokdict[idx]])
+    
+    def __setitem__(self, k, v, isinstance = isinstance) -> None:
+        if isinstance(v, _ParseResultsWithOffset):
+            self._tokdict[k] = self._tokdict.get(k, list()) + [v]
+            sub = v[0]
+        
+        elif isinstance(k, (int, slice)):
+            self._toklist[k] = v
+            sub = v
+        
+        else:
+            self._tokdict[k] = self._tokdict.get(k, []) + [_ParseResultsWithOffset(v, 0)]
+            sub = v
+        
+        if isinstance(sub, ParseResults):
+            sub._parent = self
+    
+    def __delitem__(self, idx) -> None:
+        if not isinstance(idx, (int, slice)):
+            del self._tokdict[idx]
+            return
+        
+        _len = len(self._toklist)
+        del self.toklist[idx]
+        
+        if isinstance(idx, int):
+            if idx < 0:
+                idx += _len
+            
+            idx = slice(idx, idx + 1)
+        
+        removed = list(range(*idx.indices(_len)))
+        removed.reverse()
+        
+        for occurences in self._tokdict.vaules():
+            for j in removed:
+                for k, (val, pos) in enumerate(occurences):
+                    occurences[k] = _ParseResultsWithOffset(val, pos - (pos > j))
+    
+    def __contains__(self, k) -> bool:
+        return k in self._tokdict
+    
+    def __len__(self) -> int:
+        return len(self._toklist)
+    
+    def __bool__(self) -> bool:
+        return not not (self._toklist or self._tokdict)
+    
+    def __iter__(self) -> Iterator:
+        return iter(self._toklist)
+    
+    def __reversed__(self) -> Iterator:
+        return iter(self._toklist[::-1])
+    
+    def keys(self) -> Iterator:
+        return iter(self._tokdict)
+    
+    def values(self):
+        return (self[k] for k in self.keys())
+    
+    def extend(self, item_sq):
+        if isinstance(items_sq, ParseResults):
+            self.__iadd__(itemseq)
+        
+        else:
+            self._toklist.extend(itemseq)
+    
+    def clear(self) -> None:
+        del self._toklist[:]
+        self._tokdict.clear()
+    
+    def pprint(self, *args, **kwargs) -> None:
+        pprint.pprint(self.as_list(), *args, **kwargs)
+    
+    def __add__(self, other: ParseResults) -> ParseResults:
+        ret = self.copy()
+        ret += other
+        return ret
+    
+    def __iadd__(self, other: ParseResults) -> ParseResults:
+        if not other:
+            return self
+        
+        if other._tokdict:
+            offset = len(self._toklist)
+            add_off = lambda a: offset if a < 0 else a + offset
+            other_items = other._tokdict.items()
+            other_dict_itmes = [(k, _ParseResultsWithOffset(v[0], add_off(v[1]))) for k, v_l in other_items for v in v_l]
+            
+            for k, v in other_dict_items:
+                self[k] = v
+                if isinstance(v[0], ParseResults):
+                    v[0]._parent = self
+        
+        self._toklist += other._toklist
+        self._all_names |= other._all_names
+        return self
+    
+    def __radd__(self, other) -> ParseResults:
+        '''Useful for merging many objects of type ``ParseResults`` using the sum() builtin.'''
+        if isinstance(other, int) and not other:
+            return self.copy()
+        
+        else:
+            # Might raise a TypeError, but not really anything can be done about it.
+            return other + self
+    
+    def __repr__(self) -> str:
+        return f'{type(self).__name__}({self._toklist!r}, {self.as_dict()})'
+    
+    def as_list(self, *, flatten: bool = False) -> list:
+        '''
+        Parse the results as a nested list of matching tokens, all converted to strings.
+        If flatten == True, all the nesting levels in the list are collapsed
+        '''
+        def flattend(pr):
+            to_visit = collections.deque([*self])
+            while to_visit:
+                to_do = to_visit.popleft()
+                if isinstance(to_do, ParseResults):
+                    to_visit.extendleft(to_do[::-1])
+                
+                else:
+                    yield to_do
+            
+        if flatten:
+            return [*flattend(self)]
+        
+        else:
+            return [res.as_list() if isinstance(res, ParseResults) else res for res in self._toklist]
+    ...
+
+MutableMapping.register(ParseResults)
+MutableSequence.register(ParseResults)
